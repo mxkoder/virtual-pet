4bd09c3e489a30aae7e8648950efb5ef6974ec1b
Agnes Beviz <agnes.beviz@gmail.com>
2022-06-12 17:56:15 +0100

    added hunger and fitness to growUp function

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -15,12 +15,22 @@ describe('constructor', () => {
     it('has an age property with an initial value of 0', () => {
       const pet = new Pet('Fido');
       expect(pet.age).toEqual(0);
-
     });
+
+    it('has a hunger property with an initial value of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+
+    it('has a fitness property with an initial value of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.fitness).toEqual(10);
+    });
+
   });
 
 
-describe('constructor', () => {
+describe('growUp', () => {
     it('increments age by 1', () => {
       const pet = new Pet('Fido');
       pet.growUp();
@@ -29,4 +39,25 @@ describe('constructor', () => {
       pet.growUp();
       expect(pet.age).toEqual(2);
     });
+
+    it('increments hunger by 5', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.hunger).toEqual(5);
+
+      pet.growUp();
+      expect(pet.hunger).toEqual(10);
+    });
+
+    it('decrements fitness by 3', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.fitness).toEqual(7);
+
+      pet.growUp();
+      expect(pet.fitness).toEqual(4);
+    });
+
   });
+
+
