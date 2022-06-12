f8025fb854c7adf0cc825639cf9190525011af52
Agnes Beviz <agnes.beviz@gmail.com>
2022-06-12 17:41:02 +0100

    growUp function which increments the pet object age by 1

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -11,4 +11,22 @@ describe('constructor', () => {
       const pet = new Pet('Fido');
       expect(pet.name).toEqual('Fido');
     });
+
+    it('has an age property with an initial value of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+
+    });
+  });
+
+
+describe('constructor', () => {
+    it('increments age by 1', () => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+
+      pet.growUp();
+      expect(pet.age).toEqual(2);
+    });
   });
