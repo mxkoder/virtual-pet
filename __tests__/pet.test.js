73f4f0cb4130113cb24b3a95af050cc9845b5e2b
Agnes Beviz <agnes.beviz@gmail.com>
2022-06-12 12:55:16 +0100

    added functionality to set the pet name property

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,9 +1,14 @@
 
-
 const Pet = require('../src/pet');
+
 
 describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+
+    it('sets the name property - returns an object with the name property passed in as an argument', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido');
     });
   });
